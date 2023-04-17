fef66891efa61682ce4135d3638074c5ea43de65
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-17 12:42:51 -0300

    feat: setting up challenge environment

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import { ConfigProvider } from 'antd';
+import ptBR from 'antd/lib/locale/pt_BR';
+
+import 'antd/dist/reset.css';
+
+const rootElement = document.getElementById('root') as HTMLElement;
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
+  <ConfigProvider locale={ptBR}>
     <App />
-  </React.StrictMode>
+  </ConfigProvider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
