ecf43c9cafe538eb435361c97cb0feaf2b6edfc0
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-17 19:37:42 -0300

    feat: default Selected Keys

--- src/components/AppMenu/index.tsx
+++ src/components/AppMenu/index.tsx
@@ -1,4 +1,4 @@
-import { useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 import { Menu } from 'antd';
 import { QrcodeOutlined, WindowsOutlined } from '@ant-design/icons';
 import getItem, { MenuItem } from '../../utils/menu';
@@ -11,13 +11,14 @@ const items: MenuItem[] = [
 ];
 
 export default function AppMenu() {
+  const { pathname } = useLocation();
   const navigate = useNavigate();
 
   return (
     <Menu
       theme="dark"
       mode="horizontal"
-      defaultSelectedKeys={['/dashboard']}
+      defaultSelectedKeys={[pathname]}
       onClick={(item) => navigate(item.key)}
       items={items}
     />
