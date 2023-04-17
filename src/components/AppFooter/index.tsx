05044ff5abbd85f5db615b1f41be8999c2509815
Higor Maranhão <higor.maranhao2000@gmail.com>
2023-04-17 14:05:33 -0300

    feat: footer component

---
+++ src/components/AppFooter/index.tsx
@@ -0,0 +1,9 @@
+import { Footer } from 'antd/es/layout/layout';
+
+export default function AppFooter() {
+  return (
+    <Footer style={{ textAlign: 'center' }}>
+      Ultracar challenge ©2023 Created by Higor Maranhão
+    </Footer>
+  );
+}
